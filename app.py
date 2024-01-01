dc31f638092858f51d1b0f829f35412dbf4f48d9
Adarsh Gourab Mahalik <gourabmahalikadarsh@gmail.com>
2024-01-01 12:55:43 +0530

    rebase 7

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-# app.py
 from flask import Flask, render_template, request, redirect, url_for
 import random
 import string
-import os
-import re
 
 app = Flask(__name__)
 
@@ -23,22 +20,26 @@ def is_valid_url(url):
     # Actual regex for URL matching (starts with http:// or https://)
     return bool(re.match(r'^https?://', url))
 
+# Get the server hostname dynamically
+import socket
+hostname = socket.gethostname()
+
 @app.route('/')
 def index():
-    return render_template('index.html', short_url=None)
+    return render_template('index.html', hostname=hostname, short_url=None, error=None)
 
 @app.route('/shorten', methods=['POST'])
 def shorten_url():
     original_url = request.form.get('original_link')
 
     if not original_url or not is_valid_url(original_url):
-        return render_template('index.html', short_url=None)
+        return render_template('index.html', hostname=hostname, short_url=None, error='Invalid URL')
 
     short_url = generate_short_url()
     url_database[short_url] = original_url
 
     complete_short_url = url_for('redirect_to_original', short_url=short_url, _external=True)
-    return render_template('index.html', short_url=complete_short_url)
+    return render_template('index.html', hostname=hostname, short_url=complete_short_url, error=None)
 
 @app.route('/<short_url>')
 def redirect_to_original(short_url):
@@ -47,7 +48,7 @@ def redirect_to_original(short_url):
     if original_url:
         return redirect(original_url)
     else:
-        return 'URL not found', 404
+        return render_template('index.html', hostname=hostname, short_url=None, error='URL not found')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
